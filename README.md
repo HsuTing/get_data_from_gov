051684e17e7220db07923b1a078d44234e9a7b82
HsuTing <hsuting0106@gmail.com>
2015-04-27 14:44:51 +0800

    final

---
+++ README.md
@@ -0,0 +1,11 @@
+operation:
+
+	python get_data.py
+
+output:
+
+	output.json
+
+url:
+
+	If you want to chnage url, change 'http://opendata.epa.gov.tw/ws/Data/RainTenMin/?$format=json' to your url.
--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 import urllib2
 content = urllib2.urlopen('http://opendata.epa.gov.tw/ws/Data/RainTenMin/?$format=json')
 data = content.read()
-
-print(data)
+fout = open('output.json', 'w+')
+fout.write(data)
