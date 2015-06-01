f29ee07a4b8558161713ca19d530b3b8cdc26475
HsuTingHsiang <F74011140@mail.ncku.edu.tw>
2015-06-01 21:56:47 +0800

    add time

--- get_data.py
+++ get_data.py
@@ -1,9 +1,12 @@
 import urllib2
+import time
 
 def get_data(url):
 	for i in range(0, len(url)):
+		temptime = time.localtime(time.time())
+		output = str(temptime.tm_year) + '_' + str(temptime.tm_mon) + '_' + str(temptime.tm_mday) + '_' + str(temptime.tm_hour) + '_' + str(temptime.tm_min) + '_' + str(temptime.tm_sec)
 		temp = 'http://opendata.epa.gov.tw/ws/Data/' + url[i] + '/?$format=json'
 		content = urllib2.urlopen(temp)
 		data = content.read()
-		fout = open(url[i] + '.json', 'w+')
+		fout = open(output + '_' + url[i] + '.json', 'w+')
 		fout.write(data)
